f71c24906c85128472c0596638c903ca0a14762b
N_i_v <280301@via.dk>
2021-10-22 02:02:09 +0200

    hm

--- Session4_Blazor/DefaultBlazor/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ Session4_Blazor/DefaultBlazor/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace DefaultBlazor.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 1 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 2 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 3 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 4 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 5 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 6 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 7 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 8 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 9 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using DefaultBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
+#line 10 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\_Imports.razor"
 using DefaultBlazor.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using DefaultBlazor.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Bruger\Desktop\VIA\SoftwareEngineer\3. Semester\DNP\DNP1\Session4_Blazor\DefaultBlazor\Shared\SurveyPrompt.razor"
+#line 12 "C:\Users\N_i_v\RiderProjects\DNP1\Session4_Blazor\DefaultBlazor\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
