6eb88f2ae4bf4832ce771a0ca334395744c86f69
SebastianBThomsen <280301@via.dk>
2021-10-26 15:06:23 +0200

    Wew

---
+++ Session8_HttpClient/TodoTutorial2_0/Data/ITodosService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoTutorial2_0.Models;
+
+namespace TodoTutorial2_0.Data
+{
+    public interface ITodosService
+    {
+        IList<Todo> GetTodos();
+        Task<Todo> AddTodoAsync(Todo todo);
+        void RemoveTodo(int todoId);
+        void Update(Todo todo);
+        Todo Get(int id);
+    }
+}
