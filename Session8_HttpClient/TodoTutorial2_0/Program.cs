d9bf884290397445fbf8903fbca91831195a6296
SebastianBThomsen <280301@via.dk>
2021-10-26 15:01:54 +0200

    ss

---
+++ Session8_HttpClient/TodoTutorial2_0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoTutorial2_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
